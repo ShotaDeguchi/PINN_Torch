a469445b71e10aa80b33891c4b9c7a6cbbc9b293
shota <shotadeguchio2o5@gmail.com>
2022-04-14 15:55:10 +0900

    add code

---
+++ 01_wave/pinn.py
@@ -0,0 +1,15 @@
+"""
+********************************************************************************
+Author: Shota DEGUCHI
+        Structural Analysis Lab. Kyushu Univ. (Feb. 15th, 2022)
+implementation of PINN - Physics-Informed Neural Network on PyTorch
+********************************************************************************
+"""
+
+import os
+import numpy as np
+import torch
+import time
+import datetime
+
+
